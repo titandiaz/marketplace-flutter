01edd41eb329db1315ec4bdd65f3cabe9892a2fe
Dany Escobar <danyescobar1995@gmail.com>
2019-11-09 00:59:43 -0500

    New validation for email

--- lib/src/pages/signUp.dart
+++ lib/src/pages/signUp.dart
@@ -179,6 +179,13 @@ Widget _crearEmail(BuildContext context) {
         if(input.isEmpty) {
           return 'Please type an email';
         }
+        String p =  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+        RegExp regExp = new RegExp(p);
+        if (regExp.hasMatch(input)) {
+          return null;
+        } else {
+          return 'The email provided is not valid.';
+        }
       },
       onSaved: (input) => _email = input,
       decoration: InputDecoration(

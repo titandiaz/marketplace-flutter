5f1190fd3d183ead53df020a2d88e8c79519c4dc
Dany Escobar <danyescobar1995@gmail.com>
2019-11-09 20:39:49 -0500

    Cart changes

--- lib/src/pages/cart_page.dart
+++ lib/src/pages/cart_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/menu_provider.dart';
+import 'package:peliculas/src/utils/icono_string_util.dart';
+import 'package:peliculas/src/widgets/banner_swiper_widget.dart';
 
 
 class CartPage extends StatefulWidget {
@@ -9,7 +11,7 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  final price = '129';
+  int index = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +20,6 @@ class _CartPageState extends State<CartPage> {
         child: AppBar(
           title: Center(
             child: Container(
-              // color: Colors.red,
               padding: EdgeInsets.only(top: 12.0),
               transform: Matrix4.translationValues(-30.0, 0.0, 0.0),
               child: Text('Shoping Bag',
@@ -41,55 +42,9 @@ class _CartPageState extends State<CartPage> {
         ),
       ),
       body: _listaP(),
-      // ListView(
-      //   children: <Widget>[
-      //     FloatingActionButton( child: Icon(Icons.exposure_zero), onPressed: null ),
-      //     Row(
-      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //       children: <Widget>[
-      //         Container(
-      //           transform: Matrix4.translationValues(0.0, 0.0, 0.0),
-      //           padding: EdgeInsets.only(top: 29.0, left: 31),
-      //           child: Text('Subtotal:',
-      //             style: TextStyle(
-      //               color: Color(0xFF9B9B9B), 
-      //               fontSize: 16.0,
-      //               fontFamily: "SF Pro Display",
-      //             ),
-      //           ),
-      //         ),
-      //         Container (
-      //           padding: EdgeInsets.only(top: 29.0, right: 27),
-      //           child: RaisedButton(
-      //             onPressed: () {},
-      //             shape: RoundedRectangleBorder(
-      //               borderRadius: BorderRadius.circular(4.0),
-      //             ),
-      //             child : Container (
-      //               height: 40.0,
-      //               width: 130.0,
-      //               child: Center(
-      //                 child: Text('Checkout',
-      //                   style: TextStyle(
-      //                     color: Colors.white, 
-      //                     fontSize: 16.0,
-      //                     fontFamily: "SF Pro Display",
-      //                     fontWeight: FontWeight.w600,
-      //                   ),
-      //                 ),
-      //               ),
-      //             ),
-      //             color: Color(0xFF323643),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ],
-      // ),
-      // _listaP(),
+      bottomNavigationBar: _botttomNavigationBar(context),
     );
   }
-
 
 
   Widget _listaP() {
@@ -107,7 +62,7 @@ class _CartPageState extends State<CartPage> {
             children: <Widget>[
               Container(
                 transform: Matrix4.translationValues(0.0, 0.0, 0.0),
-                padding: EdgeInsets.only(top: 29.0, left: 31),
+                padding: EdgeInsets.only(top: 35.0, left: 30, bottom: 41),
                 child: Text('Subtotal:',
                   style: TextStyle(
                     color: Color(0xFF9B9B9B), 
@@ -117,7 +72,7 @@ class _CartPageState extends State<CartPage> {
                 ),
               ),
               Container (
-                padding: EdgeInsets.only(top: 29.0, right: 27),
+                padding: EdgeInsets.only(top: 35.0, right: 27, bottom: 41),
                 child: RaisedButton(
                   onPressed: () {},
                   shape: RoundedRectangleBorder(
@@ -143,8 +98,6 @@ class _CartPageState extends State<CartPage> {
             ],
           ),
           ],
-          
-          // children: _listaItemsP(snapshot.data),
         );
         
       },
@@ -157,50 +110,135 @@ class _CartPageState extends State<CartPage> {
 
     data.forEach( (opt) {
       final widgetTempP = Container( 
-        padding: EdgeInsets.only(right: 27.0, left: 31.0),
+        padding: EdgeInsets.only(right: 27, left: 31.0),
         // color: Colors.red,
         child: Container(
+          padding: EdgeInsets.only(top: 20),
           // color: Colors.blue,
-          // padding: EdgeInsets.only(right: 27.0, left: 31.0),
           child: Column(
             children: <Widget>[
               ListTile(
-                title: Container(
-                  transform: Matrix4.translationValues(-17.0, 0.0, 0.0),
-                  child: Text( opt['price'],
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                  )
+                title: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      transform: Matrix4.translationValues(-16.0, -0.0, 0.0),
+                      // color: Colors.yellow,
+                      width: 111,
+                      height: 148,
+                      child: new Image.asset( opt['image'],
+                          width: 80.0,
+                          height: 100.0, 
+                          ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          // color: Colors.yellow,
+                          padding: EdgeInsets.only(bottom: 7),
+                          transform: Matrix4.translationValues(-5.0, -17.0, 0.0),
+                          child: Text( opt['price'],
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                          )
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(bottom: 21),
+                          transform: Matrix4.translationValues(30.0, -15.0, 0.0),
+                          child: Text( opt['name'],
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                          )
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(bottom: 5),
+                          transform: Matrix4.translationValues(20.0, -10.0, 0.0),
+                          child: Text('Color: ' +  opt['color'],
+                            style: TextStyle(
+                              fontSize: 13,
+                            ),
+                          )
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(bottom: 5),
+                          transform: Matrix4.translationValues(12, -10.0, 0.0),
+                          child: Text('Size: ' + opt['size'],
+                            style: TextStyle(
+                              fontSize: 13,
+                            ),
+                          )
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(bottom: 5),
+                          transform: Matrix4.translationValues(13.0, -10.0, 0.0),
+                          child: Text('Quanty: ' +  opt['quanty'],
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                          )
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
                 trailing: Container(
-                  transform: Matrix4.translationValues(22.0, 0.0, 0.0),
-                  child: Icon(Icons.close, color: Colors.black, size: 30,),
-                ),
-                
-              ),
-              ListTile(
-                title: Container(
-                  transform: Matrix4.translationValues(-17.0, 0.0, 0.0),
-                  child: Text( opt['name'],
-                  style: TextStyle(
-                    fontSize: 16,
+                  transform: Matrix4.translationValues(20.0, -30.0, 0.0),
+                  child: Container(
+                    child: IconButton(
+                    icon: Icon(Icons.close, color: Colors.black, size: 20,),
+                    onPressed: () {
+                      print('object');
+                    },
                   ),
-                  )
+                  ),
                 ),
-                // leading: Icon(Icons.account_circle, color: Colors.blue),
-                // trailing: Icon(Icons.keyboard_arrow_right, color: Colors.black, size: 30,),
               ),
-              Divider(),
+              Divider(
+                height: 60.0,
+                // color: Colors.yellow,
+              ),
             ],
           ),
         ),
         
       );
       opcionesP..add( widgetTempP);
-              // ..add( Divider());
     });
     return opcionesP;
   }
+
+  Widget _swiperBanner() {
+    return BannerSwiper(banners: [0]);
+  }
   
+  Widget _botttomNavigationBar(BuildContext context) {
+    
+    return Theme(
+      data: Theme.of(context).copyWith(
+        primaryColor: Colors.black
+      ),
+      child: BottomNavigationBar(
+        onTap: (newIndex) => setState(() => index = newIndex),
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(const IconData(0xe804, fontFamily: 'MyFlutterApp'), size: 20.0,),
+            title: Text('Inicio', style: TextStyle(fontSize: 13, color: Colors.black),),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(const IconData(0xe805, fontFamily: 'MyFlutterApp'), size: 20.0, ),
+            title: Text('Carrito', style: TextStyle(fontSize: 13, color: Colors.black),)
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(const IconData(0xe806, fontFamily: 'MyFlutterApp'), size: 20.0,),
+            title: Text('Perfil', style: TextStyle(fontSize: 13, color: Colors.black),)
+          ),
+        ],
+        
+      ),
+    );
+  }
+
 }
--- lib/src/utils/icono_string_util.dart
+++ lib/src/utils/icono_string_util.dart
@@ -4,10 +4,17 @@ import 'package:flutter/material.dart';
 final _icons = <String, IconData> {
 
   'account_circle' : Icons.account_circle,
+  'account_box' : Icons.account_box,
 
 };
 
 Icon getIcon( String nombreIcono ) {
+
+  return Icon( _icons[nombreIcono], color: Colors.black87, size: 60,);
+
+}
+
+Icon getIconC( String nombreIcono ) {
 
   return Icon( _icons[nombreIcono], color: Colors.black87, size: 60,);
 

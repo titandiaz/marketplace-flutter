273686d0c66057ed0347236ae8780667aa22e177
Dany Escobar <danyescobar1995@gmail.com>
2019-11-11 05:34:32 -0500

    WishList with tale

--- lib/src/pages/wishlist_page.dart
+++ lib/src/pages/wishlist_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:peliculas/src/providers/menu_provider.dart';
 
 
 class WishlistPage extends StatefulWidget {
@@ -17,87 +16,243 @@ class _WishlistPageState extends State<WishlistPage> {
 
   Widget _listaW() {
 
-    return FutureBuilder(
-      future: menuProviderW.cargarDataW(),
-      initialData: [],
-      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot ) {
-        return new GridView.count(
-          primary: false,
-          padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
-          crossAxisCount: 2,
-          childAspectRatio: 0.613,
-          mainAxisSpacing: 20.0,
-          crossAxisSpacing: 20.0,
-          children: _listaItemsW(snapshot.data),
-          shrinkWrap: true,
-        );
-      },
+    return new ListView(
+      children: <Widget>[
+        Table(
+          columnWidths: {1: FractionColumnWidth(0.5)},
+          children: [
+            TableRow(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 35, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.only(left: 35, top: 20),
+                      ),
+                      Container(
+                        child: Stack(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: new BorderRadius.circular(4.0),
+                              child: Image.asset('assets/Bitmap.png', fit: BoxFit.cover,),
+                            ),
+                            Container(
+                              transform: Matrix4.translationValues(0.0, -5.0, 0.0),
+                              padding: EdgeInsets.only(left: 110),
+                              child: IconButton( icon: Icon(Icons.cancel, color: Colors.white,), iconSize: 20,
+                                onPressed: () {
+                                  print('object');
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text( 'Beige Blazers', style: TextStyle( fontSize: 13, color: Color(0xFF9B9B9B),),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 9),
+                        child: Text( '129', style: TextStyle( fontSize: 13, color: Color(0xFF2D2D2D),),),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.only(top: 20),
+                      ),
+                      Container(
+                        child: Stack(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: new BorderRadius.circular(4.0),
+                              child: Image.asset('assets/Bitmap2.png', fit: BoxFit.cover,),
+                            ),
+                            Container(
+                              transform: Matrix4.translationValues(0.0, -5.0, 0.0),
+                              padding: EdgeInsets.only(left: 110),
+                              child: IconButton( icon: Icon(Icons.cancel, color: Colors.white,), iconSize: 20,
+                                onPressed: () {
+                                  print('object');
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text( 'Crimson Swade', style: TextStyle( fontSize: 13, color: Color(0xFF9B9B9B),),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 9),
+                        child: Text( '129', style: TextStyle( fontSize: 13, color: Color(0xFF2D2D2D),),),
+                      ),
+                    ],
+                  ),
+                ),
+              ]
+            ),
+            TableRow(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 35, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        child: Stack(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: new BorderRadius.circular(4.0),
+                              child: Image.asset('assets/Bitmap3.png', fit: BoxFit.cover,),
+                            ),
+                            Container(
+                              transform: Matrix4.translationValues(0.0, -5.0, 0.0),
+                              padding: EdgeInsets.only(left: 110),
+                              child: IconButton( icon: Icon(Icons.cancel, color: Colors.white,), iconSize: 20,
+                                onPressed: () {
+                                  print('object');
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text( 'Denims', style: TextStyle( fontSize: 13, color: Color(0xFF9B9B9B),),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 9),
+                        child: Text( '230', style: TextStyle( fontSize: 13, color: Color(0xFF2D2D2D),),),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        child: Stack(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: new BorderRadius.circular(4.0),
+                              child: Image.asset('assets/Bitmap.png', fit: BoxFit.cover,),
+                            ),
+                            Container(
+                              transform: Matrix4.translationValues(0.0, -5.0, 0.0),
+                              padding: EdgeInsets.only(left: 110),
+                              child: IconButton( icon: Icon(Icons.cancel, color: Colors.white,), iconSize: 20,
+                                onPressed: () {
+                                  print('object');
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text( 'Beige Blazers', style: TextStyle( fontSize: 13, color: Color(0xFF9B9B9B),),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 9),
+                        child: Text( '129', style: TextStyle( fontSize: 13, color: Color(0xFF2D2D2D),),),
+                      ),
+                    ],
+                  ),
+                ),
+              ]
+            ),
+            TableRow(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 35, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        child: Stack(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: new BorderRadius.circular(4.0),
+                              child: Image.asset('assets/Bitmap.png', fit: BoxFit.cover,),
+                            ),
+                            Container(
+                              transform: Matrix4.translationValues(0.0, -5.0, 0.0),
+                              padding: EdgeInsets.only(left: 110),
+                              child: IconButton( icon: Icon(Icons.cancel, color: Colors.white,), iconSize: 20,
+                                onPressed: () {
+                                  print('object');
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text( 'Beige Blazers', style: TextStyle( fontSize: 13, color: Color(0xFF9B9B9B),),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 9),
+                        child: Text( '129', style: TextStyle( fontSize: 13, color: Color(0xFF2D2D2D),),),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        child: Stack(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: new BorderRadius.circular(4.0),
+                              child: Image.asset('assets/Bitmap.png', fit: BoxFit.cover,),
+                            ),
+                            Container(
+                              transform: Matrix4.translationValues(0.0, -5.0, 0.0),
+                              padding: EdgeInsets.only(left: 110),
+                              child: IconButton( icon: Icon(Icons.cancel, color: Colors.white,), iconSize: 20,
+                                onPressed: () {
+                                  print('object');
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text( 'Beige Blazers', style: TextStyle( fontSize: 13, color: Color(0xFF9B9B9B),),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 9),
+                        child: Text( '129', style: TextStyle( fontSize: 13, color: Color(0xFF2D2D2D),),),
+                      ),
+                    ],
+                  ),
+                ),
+              ]
+            ),
+          ]
+        )
+      ],
     );
   }
-
-  List<Widget> _listaItemsW( List<dynamic> data) {
-
-    final List<Widget> opcionesW = [];
-
-    data.forEach( (opt) {
-      final widgetTempW = Container(
-        child: Column(
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: new BorderRadius.circular(4.0),
-                  child: Image.asset(
-                    opt['image'],
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Container(
-                  transform: Matrix4.translationValues(0.0, -5.0, 0.0),
-                  padding: EdgeInsets.only(left: 110),
-                  child: IconButton(
-                    icon: Icon(Icons.cancel, color: Colors.white,),
-                    iconSize: 20,
-                    onPressed: () {
-                      print('object');
-                    },
-                  ),
-                )
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 5),
-                  child: Text( opt['name'],
-                    style: TextStyle(
-                      fontSize: 13,
-                      color: Color(0xFF9B9B9B),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 9),
-                  child: Text( opt['price'],
-                    style: TextStyle(
-                      fontSize: 13,
-                      color: Color(0xFF2D2D2D),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      );
-      opcionesW..add( widgetTempW);
-    });
-    return opcionesW;
-  }
-
 
 }
